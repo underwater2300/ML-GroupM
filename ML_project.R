c93d317675b9b6292f47b0b36eb899b2091be7ad
Fanny <89777485+underwater2300@users.noreply.github.com>
2024-04-22 18:36:08 +0200

    Add files via upload

---
+++ ML_project.R
@@ -0,0 +1,2 @@
+data.music <- read.csv("C:/Users/Fafa/OneDrive/03 UNIL/SP24/01 Machine Learning/Project/Data/music.csv")
+data.vg <- read.csv("C:/Users/Fafa/OneDrive/03 UNIL/SP24/01 Machine Learning/Project/Data/video_games.csv")
